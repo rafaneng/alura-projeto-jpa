1812e589baf69f75b848836d13e5b70f40b775e0
rafaneng <rafaneng@gmail.com>
2021-02-10 21:02:28 -0300

    Implementacao de DAOs para responsabilidade única de interação com banco de dados

--- src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -1,22 +1,12 @@
 package br.com.alura.jpa.testes;
 
-import java.math.BigDecimal;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import br.com.alura.jpa.dao.MovimentacaoDao;
 
 public class TestaSomaDasMovimentacoes {
 	public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-        EntityManager em = emf.createEntityManager();
+       
+		MovimentacaoDao dao = new MovimentacaoDao();
         
-        String jpql = "select sum(m.valor) from Movimentacao m";
-        
-        TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
-        BigDecimal somaDasMovimentacoes = query.getSingleResult();
-        
-        System.out.println("A soma das movimentações é de " + somaDasMovimentacoes);
+        System.out.println("A soma das movimentações é de " + dao.getSomaDasMovimentacoes());
 	}
 }
